c8051c7842c44ef649f23a6ba47394734ac84916
Guillem Baruls Casadesús <gbaruls-@student.42barcelona.com>
2025-02-17 18:38:27 +0100

    solving a seg fault

--- src/command/init_functions.c
+++ src/command/init_functions.c
@@ -6,7 +6,7 @@
 /*   By: patri <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 09:23:05 by patri             #+#    #+#             */
-/*   Updated: 2025/02/17 18:12:37 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2025/02/17 18:36:20 by gbaruls-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	init_envi_list(t_env *data)
 	data->head = new_node;
 	data->tail = new_node3;
 	new_node->var_name = ft_strdup("PWD");
-	new_node->content = NULL; //directorio actual
+	new_node->content = ft_strdup("\0"); //directorio actual
 	new_node->next = new_node2;
 	new_node2->var_name = ft_strdup("SHLVL");
 	new_node2->content = ft_strdup("1");
