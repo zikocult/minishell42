8d905510eb943401835436f8d83bcd95796fcbf5
Guillem Barulls <gbarulls@gmail.com>
2025-02-18 23:43:18 +0100

    Finishing set and unset builtin

--- src/env/env_builtin.c
+++ src/env/env_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gbaruls- <gbaruls->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 17:17:54 by gbaruls-          #+#    #+#             */
-/*   Updated: 2025/02/18 18:26:27 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2025/02/18 23:36:05 by Guillem Barulls  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,16 @@ void	env_builtin(t_env *data)
 
 void	add_elem(t_env *data, char *name, char *content)
 {
-	data->tail->var_name = ft_strdup(name);
-	data->tail->content = ft_strdup(content);
-	insert_blank_node(data);
+	t_var	*new_node;
+
+	if (!env_search(data, name))
+	{
+		new_node = (t_var *)malloc(sizeof(t_var));
+		if (!new_node)
+			return ;
+		new_node->var_name = ft_strdup(name);
+		new_node->content = ft_strdup(content);
+		new_node->next = data->head;
+		data->head = new_node;
+	}
 }
