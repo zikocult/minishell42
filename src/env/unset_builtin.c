1e94edce14b168c77b4c3675a4b9a2e3bddd5dd0
Guillem Barulls <gbarulls@gmail.com>
2025-02-19 21:25:12 +0100

    Cleanning the directory

--- src/env/unset_builtin.c
+++ src/env/unset_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gbaruls- <gbaruls->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 19:40:22 by gbaruls-          #+#    #+#             */
-/*   Updated: 2025/02/18 17:43:09 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2025/02/19 21:15:19 by Guillem Barulls  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_var	*env_prev_search(t_env *data, char *str)
 	t_var	*current;
 
 	current = data->head;
+	if (ft_strcmp(current->var_name, str) == 0)
+		return (current);
 	while (current->next != NULL)
 	{
 		if (ft_strcmp(current->next->var_name, str) == 0)
@@ -32,11 +34,19 @@ void	unset_builtin(t_env *data, char *str)
 	t_var	*current;
 
 	current = env_prev_search(data, str);
-	del_node = NULL;
 	if (current)
 	{
-		del_node = current->next;
-		current->next = current->next->next;
+		if (current == data->head)
+			del_node = current;
+		else
+		{
+			del_node = current->next;
+			current->next = current->next->next;
+		}
+		if (del_node == data->head_env)
+			data->head_env = del_node->next;
+		if (del_node == data->head)
+			data->head = del_node->next;
 		free(del_node->content);
 		free(del_node->var_name);
 		free(del_node);
