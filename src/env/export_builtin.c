22bba410ef58cec0b2199514d4681da3a410e67b
Guillem Barulls <gbarulls@gmail.com>
2025-02-21 13:11:24 +0100

    Export begin

---
+++ src/env/export_builtin.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_builtin.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Guillem Barulls <Guillem Barulls>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 12:39:26 by Guillem Barulls   #+#    #+#             */
+/*   Updated: 2025/02/21 12:56:54 by Guillem Barulls  ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static char	*change_null(char *content, char *str)
+{
+	if (!content)
+		return (" ");
+	return (str);
+}
+
+void	export_list_builtin(t_env *data)
+{
+	t_var	*current;
+
+	current = data->head;
+	while (current->next)
+	{
+		if (!ft_strcmp(current->type, "env")
+			|| !ft_strcmp(current->type, "exp"))
+			printf("declare -x %s%s\"%s\"\n", current->var_name,
+		  			change_null(current->content, "="),
+		  			change_null(current->content, current->content));
+		current = current->next;
+	}
+}
