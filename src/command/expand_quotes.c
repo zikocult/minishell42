45a19eb8909a79dcb7e2d97cfaf2dfaf2700a45a
Patricia Manzanas Tomas <pamanzan@car12s1.42barcelona.com>
2025-02-27 18:44:50 +0100

    solving multip

--- src/command/expand_quotes.c
+++ src/command/expand_quotes.c
@@ -6,7 +6,7 @@
 /*   By: pamanzan <pamanzan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 20:05:43 by pamanzan          #+#    #+#             */
-/*   Updated: 2025/02/24 17:29:16 by pamanzan         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:44:23 by pamanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,61 @@ int	double_simple_dollar(char **str, t_env *data)
 	return (0);
 }
 
+static int	mult_dollar(char *str)
+{
+	int	i;
+	int	dollar;
+
+	dollar = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			dollar++;
+		i++;
+	}
+	printf("hola dollar\n");
+	return (dollar);
+}
+
+
+static int	expand_mult(char *str, t_env *data)
+{
+	int		j;
+	int		k;
+	char	*temp;
+	char	*expansion;
+	char	**dollars;
+
+	i = dollar_search(str);
+	temp = "\0";
+	dollars = ft_split(str, '$');
+	j = 0;
+	while(dollars[j])
+	{
+		k = 0;
+		test = ft_calloc(ft_strlen(dollars[j]) + 1, sizeof (char *));
+		test[0] = '$'; 
+		while(ft_isalnum(dollar[j][k]))
+		{
+			test[k + 1] = *dollar[j][k];
+			k++;
+		}			
+		expansion = expand_variable(&test, data);
+		if (!expansion)
+			continue;
+		box = ft_strjoin(temp, expansion);
+		free(temp);
+		temp = ft_strjoin(box, dollar[j]+k);
+		free(box);
+		j++;
+	}
+	free(*str);
+	*str = temp;
+	free(temp);
+	return (0);
+}
+
 int	double_quotes_dollar(char **str, t_env *data)
 {
 	char	*new_str;
@@ -76,12 +131,22 @@ int	double_quotes_dollar(char **str, t_env *data)
 		return (0);
 	if ((*str)[0] == '\"' && (*str)[len - 1] == '\"')
 	{
-		while ((*str)[i] == 32)
-			i++;
-		new_str = remove_double_quotes(*str);
-		handle_dollar(&new_str, data);
-		*str = new_str;
-		return (1);
+
+		if (mult_dollar(*str)> 1)
+		{
+			printf("hola\n");
+			expand_mult(str, data);
+			return (1);
+		}
+		else
+		{	
+			while ((*str)[i] == 32)
+				i++;
+			new_str = remove_double_quotes(*str);
+			handle_dollar(&new_str, data);
+			*str = new_str;
+			return (1);
+		}
 	}
 	return (0);
 }
