d75fd117298cc4a6b5c1176517dd8cc20c088947
PatriPomes <pmanzanastomas@gmail.com>
2025-03-23 22:22:35 +0100

    limpio extra pipe

--- src/pipex/extra_pipe.c
+++ src/pipex/extra_pipe.c
@@ -6,7 +6,7 @@
 /*   By: pamanzan <pamanzan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 11:08:35 by pamanzan          #+#    #+#             */
-/*   Updated: 2025/03/17 18:00:22 by pamanzan         ###   ########.fr       */
+/*   Updated: 2025/03/23 22:21:32 by patri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,17 @@
 
 int count_pipes(t_parse *state)
 {
-    int count = 0;
-    t_par *current = state->head;
+    int count;
+    t_par *current;
 
-    // Recorremos la lista de comandos
+	count = 0;
+	current = state->head;
     while (current != NULL)
     {
-        if (ft_strchr(current->command, '|'))  // Si hay un pipe en el comando
-            count++;
+        count++;
         current = current->next;
     }
-
-    return count;
+    return (count - 1);
 }
 
 char **get_argv_from_parse(t_par *current)
@@ -64,32 +63,18 @@ char **get_argv_from_parse(t_par *current)
 
 void execute_pipex(t_parse *state, t_env *data)
 {
-	int				num_pipes;
-	int				**pipes;
-	// Contar número de comandos en la lista enlazada
-	t_par *current = state->head;
-	int num_commands = 0;
+	int		num_pipes;
+	int		**pipes;
+	int		num_commands;
 
-	while (current)
-	{
-		num_commands++;
-		current = current->next;
-	}
-	// Si no hay comandos, terminamos
+	num_pipes = count_pipes(state);
+	num_commands = num_pipes + 1;
 	if (num_commands < 1)
 		return;
-	// Configuración de pipes
-	num_pipes = num_commands - 1;
-	pipes = create_big_pip(num_pipes); //chekeado y ok
-
-	// Ejecutar pipeline
-	create_pipes(pipes, num_pipes); //chekeado y ok
+	pipes = create_big_pip(num_pipes);
+	create_pipes(pipes, num_pipes);
 	execute_command(state, data, pipes);	
 	close_pipes(pipes, num_pipes);
 	wait_for_children(num_commands);
-
-	// Liberar memoria
-//	free(cmd_data.find);
 	free_pipes(pipes, num_pipes);
-//	free_memory(cmd_data.argv);
 }	
